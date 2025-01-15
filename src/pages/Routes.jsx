81d462b0f502bb55cc6daa225a35b78005e1a188
Dev-Tadeu <tadeutrabalho26@gmail.com>
2025-01-15 19:10:40 -0300

    :bug: fix: suporte para navegadores antigos

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, Routes , Route} from "react-router-dom"
+import { BrowserRouter , Route  , Routes , HashRouter } from "react-router-dom"
 import { Home } from "./Home"
 import { Projetos } from "./Projetos"
 import { Habilidades } from "./Habilidades"
 
 const AppRoutes = () => {
+    const SupportsHistory = () => {
+        return !!(window.history && window.history.pushState);
+    }
+
+    const Router = SupportsHistory() ? BrowserRouter : HashRouter;
+
     return(
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
                 <Route exact path="/Projetos" element={<Projetos/>} />
                 <Route exact path="/Habilidades" element={<Habilidades/>} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     )
 }
 
